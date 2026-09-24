6ec6ac6e30cebbb0a616ec4eaf207b2c3fb2799d
rusbelt114-glitch <rusbelt.114@gmail.com>
2026-09-23 21:27:11 -0400

    Rediseño de barra inferior estilo burbuja M3 Expressive con indicador verde claro agua e iconos interactivos

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* global self, caches */
 // Service Worker para Sistema Universitario PWA
-const CACHE_NAME = "sistema-univ-v16";
+const CACHE_NAME = "sistema-univ-v17";
 const ASSETS = [
   "./",
   "./index.html",
